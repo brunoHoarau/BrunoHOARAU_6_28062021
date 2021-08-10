52af008f56d1ecaec80b9f09f12ebd1b99905b6a
Bruno974 <hoarau.bruno@gmail.com>
2021-08-10 16:28:42 +0200

    CSS & function to sort

--- scriptPhotographer.js
+++ scriptPhotographer.js
@@ -4,11 +4,11 @@ let paramsId = urlParams.get('id');
 
 const photographerHead = document.getElementById('photographer_header');
 const main = document.getElementsByTagName('main');
+const articleSortBy = document.getElementById('article_sortBy');
 const gallery = document.getElementById('gallery');
 const modal = document.getElementById('modal');
 let imgCard;
-let number = 0;
-
+const value = "Popularité";
 
 
 let allmedia = [];
@@ -78,13 +78,11 @@ function getDatas(datas){
 console.log(allmedia)
 
 
-function showGallery(){
-  imgCardFactory(gallery);
-  }
-
 function setDatas(){
-  showGallery();
+  sortBy(value);
 } 
+
+
 
 const elmtFactory = (nodeName, attribute, ...children) => {
   const elmt = document.createElement(nodeName)
@@ -102,6 +100,23 @@ const elmtFactory = (nodeName, attribute, ...children) => {
     return elmt;
   }
 
+  function sortBy(value){
+    if(value == 'Popularité' || "" ){
+     allmedia.sort((a,b)=> { return a.likes > b.likes ? -1 : ""})
+     imgCardFactory(gallery);
+    }
+    if(value == 'Date'){
+     allmedia.sort((a,b)=> { return a.date < b.date ? -1 : ""})
+     console.log(allmedia)
+     imgCardFactory(gallery);
+    }
+    if(value == 'Titre'){
+     allmedia.sort((a,b)=> { return a.title < b.title ? -1 : ""})
+     console.log(allmedia)
+     imgCardFactory(gallery);
+    }
+  }
+
 function showPhotographerHeader(photographer){
 
   const head = elmtFactory('section',{ class:'profile'},
@@ -113,9 +128,31 @@ function showPhotographerHeader(photographer){
   elmtFactory('p',{class:'card_price'},photographer.price+'€'),
   elmtFactory('button',{id:'card_contact'},'Contactez-moi'),
   );
-  const contactfactory = elmtFactory('section',{id:"section_contact"},);
+  const contact = elmtFactory('section',{id:"section_contact"},);
 
-  const form = elmtFactory('form', {class:'section_form'},
+  const sortBySection = elmtFactory('section', {id:'sortBy'}, 
+  )
+  const sortByLabel =   elmtFactory('label', {for:'sortSelect'},"trier par",
+    elmtFactory('select', {id:'sortSelect', name:'sortSelect', onchange:'sortBy(value)'})
+  )
+  
+
+  sortBySection.appendChild(sortByLabel);
+  articleSortBy.appendChild(sortBySection);
+
+  const sortBySelect = document.getElementById('sortSelect');
+  const arraySelect = ['Popularité', 'Date', 'Titre' ];
+  arraySelect.forEach( elmt => { let option = elmtFactory('option', {value:elmt}, elmt);
+    sortBySelect.appendChild(option);
+  })
+
+  
+
+
+
+  const form =
+    elmtFactory('form', {id:'section_form'},
+    elmtFactory('button',{id:'close_contact'}, 'X'),
     elmtFactory('label', {for:'firstname'},'Prénom'),
     elmtFactory('input', {id:'firstname', required:true},),
     elmtFactory('label', {for:'name'},'Nom'),
@@ -124,10 +161,10 @@ function showPhotographerHeader(photographer){
     elmtFactory('input', {id:'email', required:true},),
     elmtFactory('label', {for:'message'},'Votre message'),
     elmtFactory('textarea', {id:'message', required:true},),
-
+    elmtFactory('button',{ type:'submit', class:"submit_contact"},'Envoyer'),
   )
 
-  let ul = elmtFactory(
+  let ulCard = elmtFactory(
      'ul',
      {class:'card_ul'},
     )
@@ -139,30 +176,30 @@ function showPhotographerHeader(photographer){
       
       li = li.toString().replace(/[, ]+/g, " ").trim();
       photographerHead.appendChild(head);
-      photographerHead.appendChild(ul);
-      photographerHead.appendChild(contactfactory);
+      photographerHead.appendChild(contact);
+      head.appendChild(ulCard);
       const sectionContact = document.getElementById('section_contact');
       sectionContact.appendChild(form);
-      ul.innerHTML += (li);
+      ulCard.innerHTML += (li);
       const contactButton = document.getElementById('card_contact');
   contactButton.addEventListener('click', (e) => {
     sectionContact.style.display='flex';
+    sectionContact.setAttribute('class', 'section_contact');
+
     e.preventDefault()
   })
     }
     
     
-    function imgCardFactory(element){
-      let numb = 0;
-      let number = 1;
-      let totalLike;
+let number = 1;
+function imgCardFactory(element){
+      let numbLike  = 0;
+      element.innerHTML = "";
       allmedia.forEach( elmt => {
       const sectionCard = elmtFactory(
         'section',
         { class:'section_card'}
       );
-    console.log(elmt.likes)
-    // let totalLike = ;
     if( elmt.image){
       let imgCard = elmtFactory(
         'img',
@@ -173,6 +210,7 @@ function showPhotographerHeader(photographer){
         {class:'lImg_tilte'},
         elmt.title
       )
+      const cardFooter = elmtFactory('div',{ class:'card_footer'},)
       const likesGroup = elmtFactory(
         'div',
         {class:'groupHeart'},
@@ -180,24 +218,27 @@ function showPhotographerHeader(photographer){
       let likes = elmtFactory(
         'p',
         {class:'lImg_likes'},
-        elmt.likes.toString()
-      ,)
-      let heart = elmtFactory(
-        'img',
-        {src:'./public/coeur.svg',class:'lImg_svg' , value:elmt.likes, onclick:'increment('+numb+')'},
-      )
+        elmt.likes.toString(),
+        )
+        let heart = elmtFactory(
+          'img',
+          {src:'./public/coeur.svg',class:'lImg_svg' , value:elmt.likes, onclick:'increment('+numbLike+')'},
+        )
+      
+      
       sectionCard.appendChild(imgCard);
-      sectionCard.appendChild(titleImg);
-      likesGroup.appendChild(heart);
       likesGroup.appendChild(likes);
+      likesGroup.appendChild(heart);
       element.appendChild(sectionCard)
-      sectionCard.appendChild(likesGroup)
+      sectionCard.appendChild(cardFooter);
+      cardFooter.appendChild(titleImg);
+      cardFooter.appendChild(likesGroup);
 
       } else {
         let imgCard = elmtFactory(
           
         'video',
-        {class:'lImg', src:"./public/"+firstName[0]+"/"+elmt.video, class:'gallery_img', type:"video/mp4", onclick:'showLightbox('+numb+')'},
+        {class:'lImg', src:"./public/"+firstName[0]+"/"+elmt.video, class:'gallery_img', type:"video/mp4", onclick:'showLightbox('+number+')'},
         elmtFactory(
           'p',
           {class:'lImg_tilte'},
@@ -210,6 +251,7 @@ function showPhotographerHeader(photographer){
           {class:'lImg_tilte'},
           elmt.title
         )
+        const cardFooter = elmtFactory('div',{ class:'card_footer'},)
         const likesGroup = elmtFactory(
           'div',
           {class:'groupHeart'},
@@ -221,20 +263,20 @@ function showPhotographerHeader(photographer){
         ,)
         let heart = elmtFactory(
           'img',
-          {src:'./public/coeur.svg',class:'lImg_svg' , value:elmt.likes, onclick:'increment('+numb+')'},
+          {src:'./public/coeur.svg',class:'lImg_svg' , value:elmt.likes, onclick:'increment('+numbLike+')'},
         )
-        element.appendChild(imgCard);
         sectionCard.appendChild(imgCard);
-        sectionCard.appendChild(titleImg);
-        likesGroup.appendChild(heart);
-        likesGroup.appendChild(likes);
-        element.appendChild(sectionCard)
-        sectionCard.appendChild(likesGroup)
+      likesGroup.appendChild(likes);
+      likesGroup.appendChild(heart);
+      element.appendChild(sectionCard)
+      sectionCard.appendChild(cardFooter);
+      cardFooter.appendChild(titleImg);
+      cardFooter.appendChild(likesGroup);
 
         
       }
       number += 1; 
-      numb += 1; 
+      numbLike += 1; 
 
     }
     
@@ -263,52 +305,61 @@ function showPhotographerHeader(photographer){
       'X',
       ),
       elmtFactory(
-        'button',
+        'a',
       { id:'lightbox_next' , class:'button_next', onclick:'nextPrev(1)'},
       '>',
       ),
     elmtFactory(
-      'button',
+      'a',
       { id:'lightbox_prev', class:'button_prev', onclick:'nextPrev(-1)'},
       '<',
       )
   );
   galleryModal.appendChild(galleryFactory);
   
-
-  
-
+  const galleryElmt = document.getElementsByClassName('section_card');
+  const galleryImg = document.getElementsByClassName('gallery_img');
+  const commandSlider = document.getElementById('commandSlider');
+  const allHeart = document.getElementsByClassName('groupHeart');
 
   function nextPrev(n){
     showLightbox( number += n);
   }
 
-  const galleryElmt = document.getElementsByClassName('gallery_img');
-  const commandSlider = document.getElementById('commandSlider');
-  const allHeart = document.getElementsByClassName('groupHeart');
   function showLightbox(number){
+    const lightboxSection = document.getElementsByClassName('lightbox_section');
+    console.log(number);
     console.log(galleryElmt.length);
-  if(number < 1){ number = galleryElmt.length };
-  if(number>galleryElmt.length){ number = 1 };
-  for( elmt of galleryElmt){
+    if(number < 1){ number = galleryElmt.length };
+    if(number > galleryElmt.length){ number = 1 };
+    console.log(number);
+  for( elmt of lightboxSection){
     elmt.style.display = 'none';
   }
+  for( elmt of galleryElmt){
+    elmt.style.display = 'none';
+    }
   for( elmt of allHeart){
     elmt.style.display ='none'
   }
-  galleryElmt[number-1].style.display= 'block';
-  galleryModal.setAttribute('class', 'lightbox')
+  galleryElmt[number-1].style.display='flex';
+  galleryElmt[number-1].setAttribute('class', 'lightbox_section');
+  // galleryImg[number-1].setAttribute('class', 'lightbox_img');
   commandSlider.style.display= 'flex';
-  }
+  gallery.setAttribute('id', 'lightbox')
+}
 
-  function close_lightbox(){
+function close_lightbox(){
+    gallery.setAttribute('id', 'gallery')
     galleryModal.setAttribute('class','gallery');
     for( elmt of galleryElmt){
-      elmt.style.display = 'flex';
+      elmt.style.display = ''; 
+      elmt.style.position= '';
+      elmt.style.right= '';
       commandSlider.style.display= 'none';
     }
     for( elmt of allHeart){
-      elmt.style.display ='flex'
+      elmt.style.display =''
     }
   }
 
